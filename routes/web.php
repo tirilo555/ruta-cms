06b1040f5c295e9578411b57e608dcb457b93e43
Marko Muravel <marko.muravel@gmail.com>
2023-07-31 17:39:22 +0300

    some fixes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-use Illuminate\Foundation\Application;
->>>>>>> d896113 (Created modules and themes. rendering views from theme. ssr)
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
->>>>>>> d896113 (Created modules and themes. rendering views from theme. ssr)
